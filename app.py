d3c98bf47bc5be2842cd7f8edad11fe671a106d9
Puja Walke <pujawalke04@gmail.com>
2026-06-25 19:11:45 +0530

    web app version

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Hello from Docker Container!")
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"<h1>Hello from Jenkins + Docker CI/CD!</h1>")
+
+server = HTTPServer(("0.0.0.0", 8000), MyHandler)
+print("Server started on port 8000")
+server.serve_forever()
